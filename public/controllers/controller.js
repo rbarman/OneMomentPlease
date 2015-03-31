95274fa087c315697e841d89bc7d01eb5b6a59cf
Rohan Barman <Rohan1Barman@gmail.com>
2015-03-30 22:10:54 -0400

    Simple LogIn Page + LogIn Service
    need server to accept POST /LogIn

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -8,6 +8,10 @@ myApp.config(['$routeProvider',
       templateUrl:'../views/signup.html',
       controller: 'SignUpCtrl'
     }).
+    when('/LogIn',{
+      templateUrl:'../views/login.html',
+      controller: 'LogInCtrl'
+    }).
     otherwise({
       redirectTo:'/SignUp'
     });
@@ -20,16 +24,31 @@ myApp.service('SignUpService',function($http){
     $http.post('/SignUp', credentials)
      .success(function(response){
        console.log(response);
+    })
+      .error(function(response){
+        console.log(response);
     });
+  };
+});
+
+myApp.service('LogInService', function($http){
+  this.logIn = function(credentials){
+    console.log(credentials);
+    $http.post('LogIn', credentials)
+      .success(function(response){
+        console.log(response);
+      })
+      .error(function(response){
+        console.log(response);
+      });
   };
 });
 
 // controllers
 myApp.controller('SignUpCtrl',['$scope','$http','SignUpService', function($scope, $http, SignUpService) {
-	console.log("hello from SignUpCtrl!");
 
 	// $scope.credentials will store information regarding user sign up. 
-   $scope.credentials = {
+  $scope.credentials = {
     firstName: '',
     lastName: '',
     username: '',
@@ -42,4 +61,16 @@ myApp.controller('SignUpCtrl',['$scope','$http','SignUpService', function($scope
 	$scope.createNewAccount = function(){
     SignUpService.signUp($scope.credentials);
 	};
+}]);
+
+myApp.controller('LogInCtrl',['$scope','$http','LogInService', function($scope, $http, LogInService){
+  
+  // $scope.credentials will store information regarding user log in
+  $scope.credentials = {
+    username:'',
+    password:''
+  };
+  $scope.logIn = function() {
+    LogInService.logIn($scope.credentials);
+  }
 }]);
