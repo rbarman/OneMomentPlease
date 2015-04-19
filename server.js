4f9432ea0495353a66853b59de47cff29748948a
pranavpunjabi@gmail.com <pranavpunjabi@gmail.com>
2015-04-19 12:52:34 -0400

    Switching to neo4j graph db

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 //module
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
+var request = require("request");
 var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
 var jwt = require('jsonwebtoken');
+
+//Require the Neo4J module
+var neo4j = require('node-neo4j');
 
 // temp
 var secret = "secret";
 
 var app = express();
+var host = localhost; 						//database runs on local host
+var port_neo_db = 7474; 					//default port specified for neo4j in neo4j docs
 
 // Client can only get /Profile endpoint with a valid token
-	// however /Profile view is not restricted -> must do in angular
+// however /Profile view is not restricted -> must do in angular
 app.use('/Profile', expressJwt({secret: secret})); 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.set('jwtTokenSecret', secret);
 
 var omp_db_url = 'mongodb://uniqueusername:unique6password@ds061371.mongolab.com:61371/omp_db';
+
+//This is the URL where we will POST our data to fire the cypher query. This is specified in Neo4j docs.
+var httpUrlForTransaction = 'http://' + host + ':' + port_neo_db + '/db/data/transaction/commit';
+
+//Create a db object. We will using this object to work on the DB.
+db = new neo4j('http://localhost:7474');
+
 
 app.get('/Profile', function(req, res){
 
@@ -84,6 +97,27 @@ app.post('/SignUp', function(req,res){
 	});
 
 });	
+
+
+//Run raw cypher with params
+db.cypherQuery(
+  'CREATE (somebody:Person { name: {name}, from: {company}, age: {age} }) RETURN somebody',
+  {
+    name: 'Ghuffran',
+    company: 'Modulus',
+    age: ~~(Math.random() * 100) //generate random age
+  }, function (err, result) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(result.data); // delivers an array of query results
+    console.log(result.columns); // delivers an array of names of objects getting returned
+  }
+);
+
+
+
+
 
 // post request to /LogIn
 app.post('/LogIn', function(req, res){
