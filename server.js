b4a0b42f46174a441e38129b414f61c4f4608684
Rohan Barman <Rohan1Barman@gmail.com>
2015-04-20 15:04:10 -0400

    LogIn with Neo4j

--- server.js
+++ server.js
@@ -27,16 +27,16 @@ var db = require("seraph")({
 
 app.get('/Profile', function(req, res){
 
-	//TODO : add error catching
-	MongoClient.connect(omp_db_url, function(err, db){
-		var collection = db.collection('users');
-		// find document with given email and return to controller
-		collection.findOne({email : req.user.email}, function(err,document){
-			if(!err){
-				res.json(document);
-			}
-		});
-	});
+	// //TODO : add error catching
+	// MongoClient.connect(omp_db_url, function(err, db){
+	// 	var collection = db.collection('users');
+	// 	// find document with given email and return to controller
+	// 	collection.findOne({email : req.user.email}, function(err,document){
+	// 		if(!err){
+	// 			res.json(document);
+	// 		}
+	// 	});
+	// });
 
 });
 
@@ -49,7 +49,7 @@ app.post('/SignUp', function(req,res){
 	// need to check if there is a User with the email provided. 
 	var predicate = { email: req.body.email};
 	db.find(predicate, [false, 'User'],function (err, objs) {
-		
+
 		if (err) {
 			console.log(err);
 			console.log("Error on finding any nodes with given email.");
@@ -96,25 +96,26 @@ app.post('/SignUp', function(req,res){
 app.post('/LogIn', function(req, res){
 	console.log("server received post request to /LogIn");
 	console.log(req.body);
+
 	// check if email and password match
-	MongoClient.connect(omp_db_url, function (err, db) {
-
-		var collection = db.collection('users');
-
-		collection.count( {email : req.body.email, password : req.body.password}, function(err, count) {
-			
-			if(count == 0) { // no record found with the email password combination
+	var cypher = "MATCH(n) WHERE n.password ='" + req.body.password + "' AND n.email = '" + req.body.email + "' return n";
+	db.query(cypher, function(err, result){
+		if(err){
+			console.log(err);
+			console.log("Error on finding any nodes with given email and password.");
+			res.status(500).send("failure: new user not added");
+		}
+		else{
+			if(result.length == 0){
 				res.status(400).send("failure: email and password do not match");
 				console.log("failure: email and password do not match")
 			}
-			else {
-				// res.status(200).send("success: LogIn successful");
+			else{
 				console.log("success: LogIn successful");
 				var token = jwt.sign(req.body, secret, { expiresInMinutes: 60*5 });
 				res.json({ token: token });
 			}
-			
-		});
+		}
 	});
 });
 
@@ -122,4 +123,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function(){
 	console.log("http://127.0.0.1:" + port);
 })
-
