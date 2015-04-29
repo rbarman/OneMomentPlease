e792137c8b1b0699c0ed819526f6e8d0227065ba
Rohan Barman <Rohan1Barman@gmail.com>
2015-04-29 13:16:03 -0400

    added tests for /User/LogIn

--- test/test_user.js
+++ test/test_user.js
@@ -1,6 +1,7 @@
 var request = require('supertest');
 var express = require('express');
 var expect = require("chai").expect;
+var bcrypt = require('bcrypt-nodejs');
 var server = require('../api/server');
 var config = require('../api/config');
 var db = require("seraph")({
@@ -9,7 +10,7 @@ var db = require("seraph")({
 	pass : config.db_password
 });
 
-describe("These are tests regarding Signing Up", function(){
+describe("These are tests for /User/SignUp", function(){
 	// Clear the databse before each test in this block. 
 	beforeEach(function(done) {
 		var cypher = "MATCH(n) delete n";
@@ -68,5 +69,67 @@ describe("These are tests regarding Signing Up", function(){
 			answer:"t_answer"
 		})
 		.expect(200,done);
+	});
+});
+
+describe("These are tests for /User/Login", function(){
+	// Clear the databse before each test in this block. 
+	beforeEach(function(done) {
+		var cypher = "MATCH(n) delete n";
+		db.query(cypher, function(err, result) {
+			if(err){
+				console.log("unable to delete all nodes in the db!");
+				console.log(err);
+			}
+			done();
+		});
+	});
+
+	it('Should pass if server receives valid email and password and sends 200', function(done){
+		db.save({
+			email:"r@r.com",
+			password:bcrypt.hashSync("r")
+		},
+		'User',
+		function(err, node){
+			if (err){
+				console.log(err);
+				console.log("Error on saving a node.");
+				done();
+			}
+			else {
+				request(server)
+				.post('/User/LogIn')
+				.send({
+					email:"r@r.com",
+					password:"r"
+				})
+				.expect(200, done);
+			}
+		});
+	});
+
+	it('Should fail if server receives wrong email and password and sends 400', function(done){
+		db.save({
+			email:"r@r.com",
+			password:bcrypt.hashSync("r")
+		},
+		'User',
+		function(err, node){
+			if (err){
+				console.log(err);
+				console.log("Error on saving a node.");
+				done();
+			}
+			else {
+				request(server)
+				.post('/User/LogIn')
+				.send({
+					email:"r@r.com",
+					password:"x"
+				})
+				.expect(400, done);
+			}
+		});
 	});
 });
