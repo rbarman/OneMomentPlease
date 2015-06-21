749d43b60292094566b8469dc2b838cce3956e67
Rohan Barman <Rohan1Barman@gmail.com>
2015-06-21 09:07:36 -0500

    SignUpCtrl controller as

--- public/controllers/SignUpCtrl.js
+++ public/controllers/SignUpCtrl.js
@@ -1,18 +1,18 @@
 var myApp = angular.module('myApp');
 
-myApp.controller('SignUpCtrl',['$scope','UserService', function($scope, UserService) {
+myApp.controller('SignUpCtrl',['UserService', function(UserService) {
 
   // TODO : Find a better place to store this + make scope.years more dynamic(?)
-  $scope.months = ["Jan.", "Feb.", "Mar.", "Apr.", "May" , "Jun.", "Jul.", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."];
-  $scope.days = [];
+  this.months = ["Jan.", "Feb.", "Mar.", "Apr.", "May" , "Jun.", "Jul.", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."];
+  this.days = [];
   for(i = 1; i < 32; i++)
-    $scope.days.push(i);
-  $scope.years = [];
+    this.days.push(i);
+  this.years = [];
   for(i = 1900; i < 2015; i++)
-    $scope.years.push(i);
+    this.years.push(i);
 
-	// $scope.credentials will store information regarding user sign up. 
-  $scope.credentials = {
+	// this.credentials will store information regarding user sign up. 
+  this.credentials = {
     firstName: '',
     lastName: '',
     email: '',
@@ -23,8 +23,8 @@ myApp.controller('SignUpCtrl',['$scope','UserService', function($scope, UserServ
     answer:''
   };
 
-	$scope.createNewAccount = function(){
-    $scope.credentials.dob = $scope.selectedMonth + ' ' + $scope.selectedDay + ' ' + $scope.selectedYear;
-    UserService.signUp($scope.credentials);
+	this.createNewAccount = function(){
+    this.credentials.dob = this.selectedMonth + ' ' + this.selectedDay + ' ' + this.selectedYear;
+    UserService.signUp(this.credentials);
 	};
 }]);
